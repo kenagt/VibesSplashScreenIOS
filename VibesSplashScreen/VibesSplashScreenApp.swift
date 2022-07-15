836182d1528671366323faba9f07774c517fbda0
kenagt <begic_kenan@hotmail.com>
2022-07-15 22:56:44 +0200

    Initial Commit

---
+++ VibesSplashScreen/VibesSplashScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  VibesSplashScreenApp.swift
+//  VibesSplashScreen
+//
+//  Created by kenagt on 15. 7. 2022..
+//
+
+import SwiftUI
+
+@main
+struct VibesSplashScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
